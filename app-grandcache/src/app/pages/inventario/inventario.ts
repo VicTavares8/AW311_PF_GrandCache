70f70ffdcd31d2f5f68ee90a85c23b02fb22d9a9
VicTavares8 <vte80988@lasallebajio.edu.mx>
2025-12-07 21:04:16 -0600

    Implemetacion de la pestaña de Inventario

--- app-grandcache/src/app/pages/inventario/inventario.ts
+++ app-grandcache/src/app/pages/inventario/inventario.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Api } from '../../services/api';
+import { Producto } from '../../models/producto.model';
 
 @Component({
   selector: 'app-inventario',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './inventario.html',
   styleUrl: './inventario.scss',
 })
-export class Inventario {
 
+export class Inventario implements OnInit {
+  private api = inject(Api);
+
+  productos = signal<Producto[]>([]);
+  cargando = signal<boolean>(true);
+
+  ngOnInit(): void {
+    this.cargarInventario();
+  }
+
+  cargarInventario() {
+    this.cargando.set(true);
+    this.api.get('productos').subscribe({
+      next: (data) => {
+        this.productos.set(data);
+        this.cargando.set(false);
+      },
+      error: (err) => {
+        console.error('Error cargando inventario:', err);
+        this.cargando.set(false);
+      }
+    });
+  }
 }
