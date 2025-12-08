a7233634e513608b0bce9d0e5ddb4aeee2455364
VicTavares8 <vte80988@lasallebajio.edu.mx>
2025-12-07 22:00:46 -0600

    Correcion de error en descripcion de inventario; Vista de inventario funcional

--- grandcache-backend/controllers/productoController.js
+++ grandcache-backend/controllers/productoController.js
@@ -17,7 +17,7 @@ exports.getProductos = async (req, res) => {
 };
 
 exports.createProducto = async (req, res) => {
-    const { nombre, description, precio, stock, stock_min, imagen_url, categoria_id, proveedor_id } = req.body;
+    const { nombre, descripcion, precio, stock, stock_min, imagen_url, categoria_id, proveedor_id } = req.body;
 
     if (!nombre || !precio) return res.status(400).json({ message: 'Un nombre y precio son requeridos' });
 
@@ -26,7 +26,7 @@ exports.createProducto = async (req, res) => {
             `INSERT INTO productos
             (nombre, descripcion, precio, stock, stock_min, imagen_url, categoria_id, proveedor_id)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-            [nombre, description, precio, stock || 0, stock_min || 5, imagen_url, categoria_id, proveedor_id]
+            [nombre, descripcion, precio, stock || 0, stock_min || 5, imagen_url, categoria_id, proveedor_id]
         );
         res.status(201).json({ message: 'Producto creado', id: result.insertId });
     } catch (error) {
